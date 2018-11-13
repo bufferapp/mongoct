5d9764b929b51b3045d24d4d595e91f7cb04708a
David Gasquez <davidgasquez@gmail.com>
2018-11-13 16:39:44 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="mongoct",
     packages=find_packages(),
-    version="0.1.0",
+    version="0.1.1",
     description="MongoDB Change Streams tracker",
     author="David Gasquez",
     license="MIT",
